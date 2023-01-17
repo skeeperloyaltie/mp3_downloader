550502ff4ff186327238069a8c62b92d2e0e4243
Godfrey Gudah <gugod254@gmail.com>
2023-01-17 03:59:14 +0000

    update using concurrent features

--- download.py
+++ download.py
@@ -1,6 +1,7 @@
 import os
 import glob
 import youtube_dl
+import concurrent.futures
 
 # directory containing the mp3 files
 directory = '/path/to/mp3/files'
@@ -20,8 +21,24 @@ for file in mp3_files:
 # directory to save downloaded files
 download_directory = '/path/to/save/files'
 
-# download files using youtube-dl
-ydl_opts = {'outtmpl': download_directory + '/%(title)s.%(ext)s'}
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    for file_name in file_names:
-        ydl.download(['https://www.youtube.com/watch?v='+file_name])
+# function to download a file using youtube-dl
+def download_file(file_name):
+    ydl_opts = {
+        'outtmpl': download_directory + '/%(title)s.%(ext)s',
+        'quiet': True,
+        'retries': 3,
+        'continuedl': True
+    }
+    url = 'https://www.youtube.com/watch?v='+file_name
+    try:
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+        print(f'Successfully downloaded {file_name}')
+    except Exception as e:
+        print(f'Error downloading {file_name}: {e}')
+
+# use concurrent.futures to download files in parallel
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    results = [executor.submit(download_file, file_name) for file_name in file_names]
+    for f in concurrent.futures.as_completed(results):
+        pass
